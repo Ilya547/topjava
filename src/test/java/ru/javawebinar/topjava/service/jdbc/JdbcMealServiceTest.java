b0cc8b426fb1c21b73ef0125ad1fa4fab44e7512
Ilya547 <Ilya547@bk.ru>
2023-03-08 09:49:41 +0400

    implement HW_5

---
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -0,0 +1,12 @@
+package ru.javawebinar.topjava.service.jdbc;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+
+import static org.junit.Assert.*;
+import static ru.javawebinar.topjava.Profiles.JDBC;
+
+@ActiveProfiles(JDBC)
+public class JdbcMealServiceTest extends AbstractMealServiceTest {
+
+}
